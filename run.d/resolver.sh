08c4e3f2201bd61878518c2d9291fb92cdf76034
Mateusz Żółtak <zozlak@zozlak.org>
2020-03-24 13:07:35 +0100

    Temporary resolver stuff (able to resolve both old and new repo) - part 1

--- run.d/resolver.sh
+++ run.d/resolver.sh
@@ -4,7 +4,8 @@ if [ ! -d /home/www-data/docroot/resolver ]; then
     su -l www-data -c 'ln -s ../vendor /home/www-data/docroot/resolver/vendor'
 fi
 su -l www-data -c 'cp /home/www-data/docroot/vendor/acdh-oeaw/arche-resolver/.htaccess /home/www-data/docroot/resolver/.htaccess'
-su -l www-data -c 'cp /home/www-data/docroot/vendor/acdh-oeaw/arche-resolver/index.php /home/www-data/docroot/resolver/index.php'
+#su -l www-data -c 'cp /home/www-data/docroot/vendor/acdh-oeaw/arche-resolver/index.php /home/www-data/docroot/resolver/index.php'
+su -l www-data -c 'cp /home/www-data/config/temporary_resolver.php /home/www-data/docroot/resolver/index.php'
 
 CMD=/home/www-data/docroot/vendor/zozlak/yaml-merge/bin/yaml-edit.php
 CFGD=/home/www-data/config/yaml
