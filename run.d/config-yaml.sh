d11ab8007b168ca8b8d256d7a2075c92718ef2cf
Mateusz Żółtak <zozlak@zozlak.org>
2020-04-20 17:04:03 +0200

    Handle lack of config/local.yaml nicely

--- run.d/config-yaml.sh
+++ run.d/config-yaml.sh
@@ -2,6 +2,10 @@
 CMD=/home/www-data/vendor/zozlak/yaml-merge/bin/yaml-edit.php
 CFGD=/home/www-data/config/yaml
 
+if [ ! -f $CFGD/local.yaml ]; then
+  echo "$CFGD/local.yaml provided - using $CFGD/local.yaml.sample instead"
+  su -l www-data -c "$CFGD/local.yaml.sample $CFGD/local.yaml"
+fi
 rm -f /home/www-data/config/initScripts/config.yaml
 su -l www-data -c "$CMD --src $CFGD/schema.yaml --src $CFGD/uriNorm.yaml --src $CFGD/local.yaml /home/www-data/config/initScripts/config.yaml"
 
