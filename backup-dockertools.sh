1ad02a20cffc803074485a8ff10ac57ca27b7be7
Mateusz Żółtak <mzoltak@oeaw.ac.at>
2021-03-01 17:50:22 +0100

    Adapt backup procedures for storage location requiring root rights

--- backup-dockertools.sh
+++ backup-dockertools.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 mkdir -p /home/www-data/backup/docker-tools/
-pg_dump -n public -T full_text_search | gzip -9 -c > /home/www-data/backup/docker-tools/dbdump.sql.gz
+if [ "`id -u`" == "0" ]; then
+    su -l www-data -c "pg_dump -n public -T full_text_search" | gzip -9 -c > /home/www-data/backup/docker-tools/dbdump.sql.gz
+else
+    pg_dump -n public -T full_text_search | gzip -9 -c > /home/www-data/backup/docker-tools/dbdump.sql.gz
+fi
 cd /home/www-data/ && tar -czf /home/www-data/backup/docker-tools/config.tgz config
 
--- backup-incremental.sh
+++ backup-incremental.sh
@@ -1,7 +1,21 @@
 #!/bin/bash
 SDIR=/home/www-data/vendor/acdh-oeaw/arche-core
 BDIR=/home/www-data/backup
-BACKUP_FILE="$BDIR/`date +%Y-%m-%d`."
+TDIR=/home/www-data/data
+FILE="`date +%Y-%m-%d`."
+DATEFILE="$BDIR/dateFile"
 
-$SDIR/backup.php --dateFile $BDIR/dateFile --compression gzip --include skipSearch --lock skip /home/www-data/docroot/api/config.yaml ${BACKUP_FILE}tgz > ${BACKUP_FILE}log 2>&1
-sha1sum ${BACKUP_FILE}tgz > ${BACKUP_FILE}tgz.sha1
+if [ "`id -u`" == "0" ]; then
+    # if the script is run as root, normal user lacks priviledges for writing to the $BDIR
+    DATEFILE="$TDIR/dateFile"
+    cp "$BDIR/dateFile" "$DATEFILE"
+    chown www-data:www-data "$DATEFILE"
+fi
+su -l www-data -c "$SDIR/backup.php --dateFile '$DATEFILE' --compression gzip --include skipSearch --lock skip /home/www-data/docroot/api/config.yaml '$TDIR/${FILE}tgz'" > "$BDIR/${FILE}log" 2>&1
+if [ "`id -u`" == "0" ]; then
+    cp "$DATEFILE" "$BDIR/dateFile"
+    rm "$DATEFILE"
+fi
+sha1sum "$TDIR/${FILE}tgz" > "$BDIR/${FILE}tgz.sha1"
+mv "$TDIR/${FILE}tgz" "$BDIR/${FILE}tgz"
+
