4895a4cc04593d707e5136f916dc29bdbc3c13a2
Mateusz Żółtak <zozlak@zozlak.org>
2021-05-25 18:37:22 +0200

    initScripts/01-users.php fixed

--- initScripts/01-adminPswd.php
+++ initScripts/01-adminPswd.php
@@ -16,5 +16,5 @@ if (!empty($adminPswd)) {
     $cfg   = json_decode(json_encode(yaml_parse_file($cfgFile)));
     $dbCfg = $cfg->accessControl;
     $db    = new PdoDb($dbCfg->db->connStr, $dbCfg->db->table, $dbCfg->db->userCol, $dbCfg->db->dataCol);
-    $db->putUser('admin', (object), HttpBasic::pswdData($adminPswd));
+    $db->putUser('admin', (object) HttpBasic::pswdData($adminPswd));
 }
