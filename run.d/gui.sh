5273723c88732a63d440e0126a65d77e94c7e12f
Mateusz Żółtak <mzoltak@oeaw.ac.at>
2021-09-22 16:23:22 +0200

    run.d/gui.sh: optimize composer autoloader

--- run.d/gui.sh
+++ run.d/gui.sh
@@ -3,9 +3,9 @@ if [ ! -d /home/www-data/gui/web ]; then
     su -l www-data -c 'composer create-project drupal/recommended-project:9.2.0 /home/www-data/gui/tmp --no-interaction --no-install'
     su -l www-data -c 'mv /home/www-data/gui/tmp/* /home/www-data/gui/ && rmdir /home/www-data/gui/tmp'
     su -l www-data -c "sed -i -e 's|\"drupal-scaffold\" *:.*|\"drupal-scaffold\": {\"allowed-packages\": [\"acdh-oeaw/arche-gui\"],|g' /home/www-data/gui/composer.json"
-    su -l www-data -c "cd /home/www-data/gui && composer require drush/drush acdh-oeaw/arche-gui acdh-oeaw/arche-theme acdh-oeaw/arche-gui-ontology acdh-oeaw/arche-dashboard composer/installers drupal/core-composer-scaffold drupal/core-project-message drupal/core-recommended drupal/devel drupal/devel_entity_updates drupal/restui drupal/matomo --update-no-dev"
+    su -l www-data -c "cd /home/www-data/gui && composer require drush/drush acdh-oeaw/arche-gui acdh-oeaw/arche-theme acdh-oeaw/arche-gui-ontology acdh-oeaw/arche-dashboard composer/installers drupal/core-composer-scaffold drupal/core-project-message drupal/core-recommended drupal/devel drupal/devel_entity_updates drupal/restui drupal/matomo --update-no-dev && composer dump-autoload -o"
 else
-    su -l www-data -c 'cd /home/www-data/gui && composer update --no-dev'
+    su -l www-data -c 'cd /home/www-data/gui && composer update --no-dev -o'
 fi
 if [ ! -L /home/www-data/docroot/browser ]; then
     su -l www-data -c "ln -s /home/www-data/gui/web /home/www-data/docroot/browser"
