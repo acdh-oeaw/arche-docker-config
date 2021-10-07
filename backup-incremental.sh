f408bc508619aba39ca3090c916501905be51d6d
Mateusz Żółtak <mzoltak@oeaw.ac.at>
2021-10-07 13:10:01 +0200

    checkStorage.sh and backup-incremental.sh modified to output results also to docroot/status/

--- backup-incremental.sh
+++ backup-incremental.sh
@@ -2,6 +2,7 @@
 SDIR=/home/www-data/vendor/acdh-oeaw/arche-core
 BDIR=/home/www-data/backup
 TDIR=/home/www-data/data
+ODIR=/home/www-data/docroot/status
 FILE="`date +%Y-%m-%d`."
 DATEFILE="$BDIR/dateFile"
 
@@ -12,6 +13,8 @@ if [ "`id -u`" == "0" ]; then
     chown www-data:www-data "$DATEFILE"
 fi
 su -l www-data -c "$SDIR/backup.php --dateFile '$DATEFILE' --compression gzip --include all --lock skip /home/www-data/docroot/api/config.yaml '$TDIR/${FILE}tgz'" > "$BDIR/${FILE}log" 2>&1
+su -l www-data -c "mkdir -p $ODIR"
+su -l www-data -c "cp '$BDIR/${FILE}log' '$ODIR/backup_last.log'"
 if [ "`id -u`" == "0" ]; then
     cp "$DATEFILE" "$BDIR/dateFile"
     rm "$DATEFILE"
--- checkStorage.sh
+++ checkStorage.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 SDIR=/home/www-data/vendor/acdh-oeaw/arche-core
 LDIR=/home/www-data/log
+ODIR=/home/www-data/docroot/status
 
-$SDIR/checkHashes.php /home/www-data/docroot/api/config.yaml > $LDIR/checkHashes.log 2>&1
-$SDIR/cleanupStorage.php /home/www-data/docroot/api/config.yaml >> $LDIR/cleanupStorage.log 2>&1
+mkdir -p $ODIR
+$SDIR/checkHashes.php /home/www-data/docroot/api/config.yaml | tee $ODIR/checkHashes.log > $LDIR/checkHashes.log 2>&1
+$SDIR/cleanupStorage.php /home/www-data/docroot/api/config.yaml | tee $ODIR/cleanupStorage.log >> $LDIR/cleanupStorage.log 2>&1
 
