d622e2ca284f9b47e9286849a45757ac82fe586e
Norbert Czirjak <Norbert.Czirjak@oeaw.ac.at>
2022-08-18 13:53:51 +0200

    Add acdh-oeaw/arche-openaire plugin along with its configuration
    (also move doorkeeper handlers definitions from yaml/repo.yaml to yaml/doorkeeper.yaml)

--- run.d/config-yaml.sh
+++ run.d/config-yaml.sh
@@ -10,7 +10,7 @@ rm -f /home/www-data/config/initScripts/config.yaml
 su -l www-data -c "$CMD --src $CFGD/schema.yaml --src $CFGD/local.yaml /home/www-data/config/initScripts/config.yaml"
 
 rm -f /home/www-data/docroot/api/config.yaml $CFGD/config-repo.yaml
-su -l www-data -c "$CMD --src $CFGD/schema.yaml --src $CFGD/repo.yaml --src $CFGD/doorkeeper.yaml --src $CFGD/local.yaml $CFGD/config-repo.yaml"
+su -l www-data -c "$CMD --src $CFGD/schema.yaml --src $CFGD/repo.yaml --src $CFGD/doorkeeper.yaml --src  $CFGD/openaire.yaml --src $CFGD/local.yaml $CFGD/config-repo.yaml"
 su -l www-data -c "sed -i -e 's/{PG_HOST}/$PG_HOST/g' -e 's/{PG_PORT}/$PG_PORT/g' -e 's/{PG_DBNAME}/$PG_DBNAME/g' -e 's/{PG_USER_PREFIX}/$PG_USER_PREFIX/g' $CFGD/config-repo.yaml"
 su -l www-data -c "ln -s $CFGD/config-repo.yaml /home/www-data/docroot/api/config.yaml"
 
