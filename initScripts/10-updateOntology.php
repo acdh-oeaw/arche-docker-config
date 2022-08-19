d07ca1d43348877d3a488bdecb7575b77cc3ac31
Norbert Czirjak <Norbert.Czirjak@oeaw.ac.at>
2022-08-19 16:35:07 +0200

    initScripts/10-updateOntology.php and initScripts/40-updateVocabularies.php rewritten with arche-schema-ingest v3

--- initScripts/10-updateOntology.php
+++ initScripts/10-updateOntology.php
@@ -1,52 +1,32 @@
 #!/usr/bin/php
 <?php
 /*
- * Checks if the ontology is present in the repo and up to date.
- * If not, imports it.
+ * Assures most recent ontology version is ingested.
+ *
+ * All runtime parameters are passed to the arche-import-ontology script
  */
 
 require_once '/home/www-data/vendor/autoload.php';
-use acdhOeaw\arche\lib\Repo;
-use acdhOeaw\arche\lib\exception\NotFound;
 
-$cfgFile      = __DIR__ . '/config.yaml';
-$ontologyFile = '/home/www-data/vendor/acdh-oeaw/arche-schema/acdh-schema.owl';
+$cfg = yaml_parse_file(__DIR__ . '/config.yaml')['auth']['httpBasic'];
+$user = escapeshellarg($cfg['user']);
+$pswd = escapeshellarg($cfg['password']);
+$args = implode(' ', array_map('escapeshellarg', array_slice($argv, 1)));
 
-$cfg                     = yaml_parse_file($cfgFile);
-$cfg['composerLocation'] = '/home/www-data/vendor/autoload.php';
-yaml_emit_file($cfgFile, $cfg);
+setDoorkeeperChecks(false);
+echo "Importing ontology\n";
+system("/home/www-data/vendor/bin/arche-import-ontology --user $user --pswd $pswd --concurrency 6 $args http://127.0.0.1/api");
+setDoorkeeperChecks(true);
 
-$repo    = Repo::factory($cfgFile);
-$import  = true;
-try {
-    $res    = $repo->getResourceById(preg_replace('/#$/', '', $cfg['schema']['namespaces']['ontology']));
-    $hash   = (string) $res->getGraph()->getLiteral($cfg['schema']['hash']);
-    $md5    = 'md5:' . md5_file($ontologyFile);
-    $sha1   = 'sha1:' . sha1_file($ontologyFile);
-    $import = !in_array($hash, [$md5, $sha1]);
-} catch (NotFound $e) {
+// helper functions
 
-}
-
-if ($import || ($argv[1] ?? false)) {
-    // turn off some doorkeeper checks
+function setDoorkeeperChecks(bool $on): void {
     $sCfgFile = __DIR__ . '/../yaml/config-repo.yaml';
+    static $dCfgBak = [];
     $sCfg     = yaml_parse_file($sCfgFile);
     $dCfgBak  = $sCfg['doorkeeper'];
-    $sCfg['doorkeeper']['checkUnknownProperties']    = false;
-    $sCfg['doorkeeper']['checkAutoCreatedResources'] = false;
-    $sCfg['doorkeeper']['checkVocabularyValues']     = false;
+    $sCfg['doorkeeper']['checkUnknownProperties']    = $on ? $dCfgBak['checkUnknownProperties'] : false;
+    $sCfg['doorkeeper']['checkAutoCreatedResources'] = $on ? $dCfgBak['checkAutoCreatedResources'] : false;
+    $sCfg['doorkeeper']['checkVocabularyValues']     = $on ? $dCfgBak['checkVocabularyValues'] : false;
     yaml_emit_file($sCfgFile, $sCfg);
-
-    echo "Importing ontology\n";
-    system("php -f /home/www-data/vendor/acdh-oeaw/arche-schema-ingest/importOntology.php $cfgFile $ontologyFile --skipVocabularies");
-    
-    // restore doorkeeper checks
-    $sCfg['doorkeeper']['checkUnknownProperties']    = $dCfgBak['checkUnknownProperties'];
-    $sCfg['doorkeeper']['checkAutoCreatedResources'] = $dCfgBak['checkAutoCreatedResources'];
-    $sCfg['doorkeeper']['checkVocabularyValues']     = $dCfgBak['checkVocabularyValues'];
-    yaml_emit_file($sCfgFile, $sCfg);
-} else {
-    echo "Ontology up to date\n";
 }
-
--- initScripts/40-updateVocabularies.php
+++ initScripts/40-updateVocabularies.php
@@ -1,28 +1,20 @@
 #!/usr/bin/php
 <?php
 /*
- * Updates external vocabularies mentioned in the ontology
+ * Updates external vocabularies used by the ontology
+ *
+ * All runtime parameters are passed to the arche-import-vocabularies script
  */
 
 require_once '/home/www-data/vendor/autoload.php';
 
-$cfgFile                  = __DIR__ . '/config.yaml';
-$cfg                     = yaml_parse_file($cfgFile);
-$cfg['composerLocation'] = '/home/www-data/vendor/autoload.php';
-yaml_emit_file($cfgFile, $cfg);
-
-// turn off some doorkeeper checks
-$sCfgFile = __DIR__ . '/../yaml/config-repo.yaml';
-$sCfg     = yaml_parse_file($sCfgFile);
-$dCfgBak  = $sCfg['doorkeeper'];
-$sCfg['doorkeeper']['checkUnknownProperties']    = false;
-$sCfg['doorkeeper']['checkAutoCreatedResources'] = false;
-yaml_emit_file($sCfgFile, $sCfg);
+$cfg = yaml_parse_file(__DIR__ . '/config.yaml')['auth']['httpBasic'];
+$user = escapeshellarg($cfg['user']);
+$pswd = escapeshellarg($cfg['password']);
+$args = implode(' ', array_map('escapeshellarg', array_slice($argv, 1)));
 
 echo "Importing external vocabularies\n";
-system("php -f /home/www-data/vendor/acdh-oeaw/arche-schema-ingest/importVocabularies.php $cfgFile");
+# all arche: properties are skipped because the quite often don't follow the current ontology
+system("/home/www-data/vendor/bin/arche-import-vocabularies http://127.0.0.1/api --user $user --pswd $pswd --concurrency 6 --makePublic --dropHasTopConcept --allowedNmsp 'http://www.w3.org/2004/02/skos/core#' 'http://purl.org/dc/' $args");
 
-$sCfg['doorkeeper']['checkUnknownProperties']    = $dCfgBak['checkUnknownProperties'];
-$sCfg['doorkeeper']['checkAutoCreatedResources'] = $dCfgBak['checkAutoCreatedResources'];
-yaml_emit_file($sCfgFile, $sCfg);
 
