df25fbbdecde1ff61e637d425baafbef4b77e41e
Norbert Czirjak <Norbert.Czirjak@oeaw.ac.at>
2023-03-09 20:51:20 +0100

    updateFromRefSources.sh and its config added

---
+++ updateFromRefSources.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+SDIR=/home/www-data/vendor/bin
+LDIR=/home/www-data/log
+ODIR=/home/www-data/docroot/status
+CDIR=`dirname "$BASH_SOURCE[0]"`
+
+PSWD=`grep "password:" "$CDIR/initScripts/config.yaml" | head -n 1 | sed -E 's/^.*password: *"?(.*)"? *$/\1/g'`
+mkdir -p $ODIR
+$SDIR/arche-ref-sources --output "$ODIR/refSources.ttl" --user init --pswd "$PSWD" --mode update --verbose --afterFile "$CDIR/yaml/ref-sources.lastDate" "$CDIR/yaml/ref-sources.yaml" 2>&1 > "$ODIR/refSources.log"
