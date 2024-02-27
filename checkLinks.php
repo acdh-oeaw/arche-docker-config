d0196bcef61103ea6f4376757a879d57d8ae9f8c
Mateusz Żółtak <mzoltak@oeaw.ac.at>
2024-02-27 09:44:44 +0100

    checkLinks.php: --auth and --authNmsp added allowing to use HTTP basic auth in a given namespace

--- checkLinks.php
+++ checkLinks.php
@@ -6,10 +6,10 @@ use GuzzleHttp\Pool;
 
 include '/home/www-data/vendor/autoload.php';
 
-$param = ['dbConn' => 'pgsql:', 'timeout' => 10, 'parallel' => 5, 'retry400WithGet' => false, 'help' => false, 'progress' => false];
-$helpStr = "$argv[0] [--timeout seconds] [--parallel N] [--dbConn PDOconnString] [--retryGet] [--help]\n\nSearches arche-core database for broken URLs.\nAll literal values of type xsd:anyURI are checked.\n\ndefault parameter values: dbConn: '" . $param['dbConn'] . "', timeout: " . $param['timeout'] . ", parallel: " . $param['parallel'] . "\n\n";
+$param = ['dbConn' => 'pgsql:', 'timeout' => 10, 'parallel' => 5, 'retry400WithGet' => false, 'help' => false, 'progress' => false, 'auth' => '', 'authNmsp' => ''];
+$helpStr = "$argv[0] [--timeout seconds] [--parallel N] [--dbConn PDOconnString] [--retryGet] [--auth user:pswd] [--authNmsp authIsUsedOnlyInThisNamespace] [--help]\n\nSearches arche-core database for broken URLs.\nAll literal values of type xsd:anyURI are checked.\n\ndefault parameter values: dbConn: '" . $param['dbConn'] . "', timeout: " . $param['timeout'] . ", parallel: " . $param['parallel'] . "\n\n";
 foreach ($argv as $n => $v) {
-    if (in_array($v, ['--timeout', '--parallel', '--dbConn'])) {
+    if (in_array($v, ['--timeout', '--parallel', '--dbConn', '--auth', '--authNmsp'])) {
         if (!isset($argv[$n + 1])) {
             echo $helpStr;
             exit();
@@ -25,8 +25,7 @@ if ($param['help']) {
 }
 
 $t0 = microtime(true);
-
-$client = new GuzzleHttp\Client([
+$opts = [
     'http_errors'     => false,
     'timeout'         => $param['timeout'],
     'allow_redirects' => [
@@ -34,11 +33,18 @@ $client = new GuzzleHttp\Client([
         'strict'          => false,
         'track_redirects' => true,
     ],
-]);
+];
+$client     = new GuzzleHttp\Client($opts);
+$clientAuth = null;
+if (!empty($param['auth'])) {
+    $opts['auth'] = explode(':', $param['auth']);
+    $clientAuth   = new GuzzleHttp\Client($opts);
+}
 $pdo = new PDO($param['dbConn']);
 $count = $pdo->query("SELECT count(DISTINCT value) FROM metadata WHERE type = 'http://www.w3.org/2001/XMLSchema#anyURI'")->fetchColumn();
-unset($param['dbConn']);
-echo "@ Checking for broken URLs (" . date('Y-m-d H:i:s') . ") $count URLs to check\n@ " . json_encode($param) . "\n\n";
+unset($param['dbConn'], $param['help']);
+$param['auth'] = !empty($param['auth']) ? '***' : '';
+echo "@ Checking for broken URLs (" . date('Y-m-d H:i:s') . ") $count URLs to check\n@ " . json_encode($param, JSON_UNESCAPED_SLASHES) . "\n\n";
 
 $urls = [];
 function fetchRequests($pdo) {
@@ -60,13 +66,21 @@ $failing = [];
 $pool = new Pool($client, fetchRequests($pdo), [
     'concurrency' => $param['parallel'], 
     'fulfilled' => function(Response $response, $index) {
-        global $urls, $broken, $param, $client;
+        global $urls, $broken, $param, $client, $clientAuth;
         $index  = (string) $index;
         $url    = $urls[$index];
         $status = $response->getStatusCode();
         if ($status === 400 && $param['retry400WithGet']) {
             $response = $client->send(new Request('GET', $url));
             $status = $response->getStatusCode();
+        }
+        if (in_array($status, [401, 403]) && $clientAuth !== null) {
+            $redirects = $response->getHeader('X-Guzzle-Redirect-History');
+            $lastUrl = count($redirects) > 0 ? end($redirects) : $url;
+            if (str_starts_with($lastUrl, $param['authNmsp'])) {
+                $response = $clientAuth->send(new Request('HEAD', $lastUrl));
+                $status   = $response->getStatusCode();
+            }
         }
         if ($status < 200 || $status >= 400) {
             if (!isset($broken[(string) $status])) {
