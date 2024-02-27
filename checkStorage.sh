2b9e151e927887c715a9a43734cbe92f0413aa8a
Mateusz Żółtak <mzoltak@oeaw.ac.at>
2024-02-27 09:48:45 +0100

    checkStorage.sh: use --auth and --authNmsp of checkLinks.php

--- checkStorage.sh
+++ checkStorage.sh
@@ -7,5 +7,5 @@ CDIR=`dirname "$BASH_SOURCE[0]"`
 mkdir -p $ODIR
 $SDIR/checkHashes.php /home/www-data/docroot/api/config.yaml | tee $ODIR/checkHashes.log > $LDIR/checkHashes.log 2>&1
 $SDIR/cleanupStorage.php /home/www-data/docroot/api/config.yaml | tee $ODIR/cleanupStorage.log >> $LDIR/cleanupStorage.log 2>&1
-$CDIR/checkLinks.php --retry400WithGet --parallel 5 --timeout 15 | tee $ODIR/checkLinks.log > $LDIR/checkLinks.log 2>&1
+$CDIR/checkLinks.php --retry400WithGet --parallel 5 --timeout 15 --auth "$1" --authNmsp "$2" | tee $ODIR/checkLinks.log > $LDIR/checkLinks.log 2>&1
 
