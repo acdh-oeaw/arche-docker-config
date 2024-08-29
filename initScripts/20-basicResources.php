4228d832760b9034c6eef5ac5aeb64663403dc0b
Mateusz Żółtak <zozlak@zozlak.org>
2024-08-29 11:13:51 +0000

    initScripts/basicResources.ttl acdhi:restrictedAccessImage added
    This is a technical resource we use to advertise over OAI-PMH to the
    kulrurpool for images with restricted access.

--- initScripts/20-basicResources.php
+++ initScripts/20-basicResources.php
@@ -7,6 +7,7 @@
 
 require_once '/home/www-data/vendor/autoload.php';
 use acdhOeaw\arche\lib\Repo;
+use acdhOeaw\arche\lib\BinaryPayload;
 use acdhOeaw\arche\lib\ingest\MetadataCollection;
 
 MetadataCollection::$debug = true;
@@ -14,12 +15,21 @@ $cfgFile   = __DIR__ . '/config.yaml';
 $cfg       = json_decode(json_encode(yaml_parse_file($cfgFile)));
 $repo      = Repo::factory($cfgFile);
 $graph     = new MetadataCollection($repo, __DIR__ . '/basicResources.ttl');
-$repo->begin();
+
 try {
+    echo "Importing basicResources.ttl\n";
+    $repo->begin();
     $resources = $graph->import($cfg->schema->namespaces->id, MetadataCollection::SKIP);
     $repo->commit();
+    echo "\tsuccessful\n";
+
+    echo "Importing restrictedAccessImage.png\n";
+    $repo->begin();
+    $res = $repo->getResourceById('https://id.acdh.oeaw.ac.at/restrictedAccessImage');
+    $res->updateContent(new BinaryPayload(null, __DIR__ . '/restrictedAccessImage.png', 'image/png'));
+    $repo->commit();
+    echo "\tsuccessful\n";
 } catch (Exception $e) {
     echo acdhOeaw\arche\lib\exception\ExceptionUtil::unwrap($e) . "\n";
     $repo->rollback();
 }
-
