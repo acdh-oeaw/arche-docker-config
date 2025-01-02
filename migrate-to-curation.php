90b527378e7ccf58b64af286f239ca43877ed0ec
Mateusz Żółtak <zozlak@zozlak.org>
2025-01-02 17:46:39 +0000

    migrate-to-curation.php: fixes after testing the restore
    * do not drop (and restore) the database schema
    * uncomment system() call taking the database dump
    * dump only explicitly stated database tables (resources, identifiers,
      relations, metadata, spatial_search, full_text_search)
    * use the --disable-triggers to dump the database to speed up the
      restore and avoid problems wit the table search paths within triggers
    * allow to specify dbUser during restore to make sure user with
      enough permissions is used
    * manually truncate migrated tables on restore
    * smaller fixes

--- migrate-to-curation.php
+++ migrate-to-curation.php
@@ -2,36 +2,39 @@
 <?php
 /*
  * A script for migrating the production instance metadata to the curation instance.
- * - migrates all the metadata but the history of changes,
- * - preserves users database on the curation instance
+ * - migrates all the metadata (including spatial and fts indices) but the history of metadata changes,
+ * - does not affect users database on the curation instance
  * - migrates title images
  * - takes care of the identifiers migration
+ *
+ * When running the restore, specify a dbUser being an owner of the migrated database tables.
+ * This is required to disable triggers during the import
+ * (and disabling triggers is required for the migration to work).
  */
 
 include '/home/www-data/vendor/autoload.php';
 
 if (!in_array($argv[1] ?? '', ['dump', 'restore'])) {
-    echo "\n$argv[0] [dump|restore]\n\n";
+    echo "\n$argv[0] dump\n  or\n$argv[0] restore [dbUser]\n\n";
     exit(1);
 }
 
 $cfg        = json_decode(json_encode(yaml_parse_file('/home/www-data/config/yaml/config-repo.yaml')));
 $pdo        = new PDO($cfg->dbConn->admin);
-$schemaDump = 'dump_schema.sql';
 $dataDump   = 'dump_data.sql';
 $imgsDump   = 'dump_titleimgages.tar';
-$usersDump  = 'dump_users.sql';
+$usersDump  = '/tmp/dump_users.sql';
 $cfgDump    = 'dump_config.yaml';
 
 $dbConnParam = array_map(fn($x) => explode('=', $x), explode(' ', $cfg->dbConnStr->admin));
 $dbConnParam = array_combine(array_map(fn($x) => $x[0], $dbConnParam), array_map(fn($x) => $x[1] ?? '', $dbConnParam));
-$dbConn = '' .
-    (isset($dbConnParam['host']) ? " -h '" . $dbConnParam['host'] . "'" : '') .
-    (isset($dbConnParam['port']) ? " -p '" . $dbConnParam['port'] . "'" : '') .
-    (isset($dbConnParam['user']) ? " -U '" . $dbConnParam['user'] . "'" : '') .
-    (isset($dbConnParam['dbname']) ? " '" . $dbConnParam['dbname'] . "'" : '') ;
 
 if ($argv[1] === 'dump') {
+    $dbConn = '' .
+        (isset($dbConnParam['host']) ? " -h '" . $dbConnParam['host'] . "'" : '') .
+        (isset($dbConnParam['port']) ? " -p '" . $dbConnParam['port'] . "'" : '') .
+        (isset($dbConnParam['user']) ? " -U '" . $dbConnParam['user'] . "'" : '') .
+        (isset($dbConnParam['dbname']) ? " '" . $dbConnParam['dbname'] . "'" : '') ;
     $basePath = $cfg->storage->dir;
     $levelMax = $cfg->storage->levels;
 
@@ -47,10 +50,8 @@ if ($argv[1] === 'dump') {
 
     echo "Copying repository config\n";
     copy('/home/www-data/config/yaml/config-repo.yaml', "$basePath/$cfgDump");
-    echo "Dumping schema\n";
-    #system('pg_dump -f ' . escapeshellarg("$basePath/$schemaDump") . ' -C -s ' . $dbConn);
     echo "Dumping metadata\n";
-    #system('pg_dump -f ' . escapeshellarg("$basePath/$dataDump") . ' -a -T metadata_history -T users ' . $dbConn);
+    system('pg_dump -f ' . escapeshellarg("$basePath/$dataDump") . ' -a --disable-triggers -t resources -t identifiers -t relations -t metadata -t spatial_search -t full_text_search ' . $dbConn);
 
     echo "Dumping title images\n";
     $query = $pdo->prepare("SELECT id FROM relations r JOIN metadata m USING (id) WHERE r.property = ? AND m.property = ? ORDER BY 1");
@@ -63,35 +64,48 @@ if ($argv[1] === 'dump') {
     system('tar -c -f ' . escapeshellarg("$basePath/$imgsDump") . $imgs);
     echo "\nDump completed - copy all the $basePath/dump_* files to the curation instance and run the script with the 'restore' parameter there.\n\n";
 } else {
-    foreach ([$schemaDump, $dataDump, $imgsDump, $cfgDump] as $i) {
+    foreach ([$dataDump, $imgsDump, $cfgDump] as $i) {
         if (!file_exists($i)) {
             echo "Missing dump file $i (should be in the current working directory)\n";
             exit(2);
         }
-    }     
+    }
     $user = posix_getpwuid(posix_geteuid())['name'];
     if ($user === 'root') {
         echo "Can't be run as root.\n";
         exit(3);
     }
 
-    echo "Creating users database backup\n";
-    system("pg_dump -a -t users -f $usersDump");
-    echo "Recreating the database\n";
-    system("dropdb " . escapeshellarg($user));
-    system("createdb " . escapeshellarg($user));
-    echo "Migrating schema\n";
-    system("psql -f " . escapeshellarg($schemaDump));
-    echo "Migrating data\n";
-    system("psql -f " . escapeshellarg($dataDump));
-    echo "Restoring users database\n";
-    system("psql -f " . escapeshellarg($usersDump));
+    $dbUser = $argv[2] ?? $user;
+    $dbConn = explode("\n", file_get_contents($_SERVER['HOME'] . '/.pgpass'));
+    $dbConn = array_map(fn($x) => explode(":", $x), $dbConn);
+    $dbConn = array_filter($dbConn, fn($x) => ($x[3] ?? '') === $dbUser);
+    $dbConn = reset($dbConn);
+    if (!empty($dbConn)) {
+        $dbConn = '' .
+            (isset($dbConnParam['host']) ? " -h '" . $dbConnParam['host'] . "'" : '') .
+            (isset($dbConnParam['port']) ? " -p '" . $dbConnParam['port'] . "'" : '') .
+            " -U '$dbUser'" .
+            (isset($dbConnParam['dbname']) ? " '" . $dbConnParam['dbname'] . "'" : '') ;
+    }
+
+    echo "Removing old metadata\n";
+    system("psql $dbConn -c 'TRUNCATE resources CASCADE; TRUNCATE metadata_history;'");
+    echo "Importing new data\n";
+    #system("psql -f " . escapeshellarg($dataDump) . $dbConn);
+    echo "psql -f " . escapeshellarg($dataDump) . $dbConn . "\n";
+    #system("psql -f " . escapeshellarg($usersDump) . $dbConn);
     echo "Updating base indentifiers\n";
     $oldCfg    = json_decode(json_encode(yaml_parse_file($cfgDump)));
     $oldIdBase = $oldCfg->rest->urlBase . $oldCfg->rest->pathBase;
     $newIdBase = $cfg->rest->urlBase . $cfg->rest->pathBase;
-    $query     = $pdo->prepare("INSERT INTO identifiers (ids, id) SELECT replace(ids, ?, ?), id FROM identifiers WHERE ids LIKE ?");
-    $query->execute([$oldIdBase, $newIdBase, "$oldIdBase%"]);
+    #$query     = $pdo->prepare("UPDATE identifiers SET ids = replace(ids, ?, ?) WHERE ids LIKE ?");
+    #$query->execute([$oldIdBase, $newIdBase, "$oldIdBase%"]);
+    echo "UPDATE identifiers SET ids = replace(ids, '$oldIdBase', '$newIdBase') WHERE ids LIKE '$oldIdBase%'";
+    echo "Restoring thumbnails\n";
+    $imgsDump = getcwd() . "/$imgsDump";
+    chdir($cfg->storage->dir);
+    system("tar -x -f " . escapeshellarg($imgsDump));
     echo "\nMigration finished\n\n";
 }
 
