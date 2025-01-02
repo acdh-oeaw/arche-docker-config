82238faa7cd29329d766255c1da61b2301a15757
Mateusz Żółtak <zozlak@zozlak.org>
2025-01-02 13:07:48 +0000

    * initScripts/dissServices.ttl: add iiif presentation manifest service * check.sh: copy logs to the docroot/status/ only after a given check is   completed not to trigger icinga warnings while a check is in progress * backup.sh: use arche-core's backup.php for the database backup and   few other small fixes * composer.json: bump arche-ref-sources to ^1

--- backup.sh
+++ backup.sh
@@ -1,27 +1,34 @@
 #!/bin/bash
 SDIR=/home/www-data/vendor/acdh-oeaw/arche-core
 BDIR=/home/www-data/backup
-TDIR=/home/www-data/data
+TDIR=/home/www-data/data/tmp
 ODIR=/home/www-data/docroot/status
 CDIR=`dirname "$BASH_SOURCE[0]"`
-FILE="`date +%Y-%m-%d`."
+FILE="`date +%Y-%m-%d`"
 DATEFILE="$BDIR/dateFile"
 DOW=`date +%u`
+COMPRESSION="none"
+COMPRESSIONLEVEL=9
 
 . "$CDIR/cluster_init.sh"
 
-# config dir backup (it is a git repo but what about uncommited changes)
-cd /home/www-data/ && tar -czf $BDIR/config_$DOW.tgz config && echo "--- Config archived successfully"
-if [ "$DOW" == "7" ] ; then
-    # full incremental binaries backup on Sunday
-    $SDIR/backup.php --dateFile "$DATEFILE" --compression gzip --include all --lock skip "$CFGFILE" "$TDIR/${FILE}tgz" 2>&1 | tee "$ODIR/backup_last.txt" &&\
-    sha1sum "$TDIR/${FILE}tgz" > "$BDIR/${FILE}tgz.sha1" &&\
-    mv "$TDIR/${FILE}tgz" "$BDIR/${FILE}tgz" && echo "--- Incremental binary dump ended successfully" | tee -a "$ODIR/backup_last.txt"
-else
-    # just a database on other days
-    su -l www-data -c "pg_dump -h $HOST -p $PORT -U $USER -n public -T metadata_history $DBNAME" | pv -F '%b ellapsed: %t cur: %r avg: %a' | gzip -9 -c > $BDIR/dbdump_$DOW.sql.gz && echo "--- Database dumped successfully"
+echo "### Initialization completed, starting a backup on `date +%Y-%m-%dT%H:%M:%S`"
+
+# config dir backup (it's a git repo but can't harm to backup uncommited changes)
+cd /home/www-data/ && tar -czf $BDIR/config_$DOW.tgz config && echo "### Config archived successfully"
+# always dump the database
+echo "### Dumping the database"
+$SDIR/backup.php $BDIR/dbdump_$DOW "$CFGFILE" --tmpDir "$TDIR" --include all --compression gzip --compressionLevel 9 --dateFrom 2200-01-01 --dateTo 2200-01-01 2>&1 | tee $BDIR/dbdump_$DOW.log &&\
+    echo "### Database dumped successfully"
+# full incremental binaries backup on Sunday
+if [ "$DOW" == "4" ] ; then
+    $SDIR/backup.php --dateFile "$DATEFILE" --compression $COMPRESSION --compressionLevel $COMPRESSIONLEVEL --include none --lock skip "$CFGFILE" "$TDIR/${FILE}" 2>&1 | tee "$BDIR/${FILE}.log" &&\
+    && echo "### Incremental binary dump ended successfully" | tee -a "$BDIR/${FILE}.log"
+    cp "$BDIR/${FILE}.log" "$ODIR/backup_last.txt"
 fi
 
+echo "### Backup ended on `date +%Y-%m-%dT%H:%M:%S`"
+
 # compress logs
-echo "--- Compressing logs"
+echo "### Compressing logs"
 $CDIR/run.d/01-logs.sh
--- checks.sh
+++ checks.sh
@@ -7,7 +7,10 @@ CDIR=`dirname "$BASH_SOURCE[0]"`
 
 . "$CDIR/cluster_init.sh"
 
-$SDIR/checkHashes.php "$CFGFILE" 2>&1 | tee $ODIR/checkHashes.txt $LDIR/checkHashes.log
-$SDIR/cleanupStorage.php "$CFGFILE" 2>&1 | tee $ODIR/cleanupStorage.txt $LDIR/cleanupStorage.log
-$CDIR/checkLinks.php --dbConn "$DBCONN" --retry400WithGet --parallel 5 --timeout 15 --auth "$1" --authNmsp "$2" 2>&1 | tee $ODIR/checkLinks.txt $LDIR/checkLinks.log
+$SDIR/checkHashes.php "$CFGFILE" 2>&1 | tee $LDIR/checkHashes.log
+cp $LDIR/checkHashes.log $ODIR/checkHashes.tx
+$SDIR/cleanupStorage.php "$CFGFILE" 2>&1 | tee $LDIR/cleanupStorage.log
+cp $LDIR/cleanupStorage.log $ODIR/cleanupStorage.txt
+$CDIR/checkLinks.php --dbConn "$DBCONN" --retry400WithGet --parallel 5 --timeout 15 --auth "$1" --authNmsp "$2" 2>&1 | tee $LDIR/checkLinks.log
+cp $LDIR/checkLinks.log $ODIR/checkLinks.txt
 
