22e7cf50a77229531ee180d8323bfac4099a8cbb
Mateusz Żółtak <zozlak@zozlak.org>
2025-01-02 15:48:52 +0000

    migrate-to-curation.php: fix dump
    * read db connection settings from the instance config yaml
    * fix thumbnails tar creation

--- migrate-to-curation.php
+++ migrate-to-curation.php
@@ -8,49 +8,59 @@
  * - takes care of the identifiers migration
  */
 
-include $_SERVER['HOME'] . '/vendor/autoload.php';
+include '/home/www-data/vendor/autoload.php';
 
 if (!in_array($argv[1] ?? '', ['dump', 'restore'])) {
     echo "\n$argv[0] [dump|restore]\n\n";
     exit(1);
 }
 
-$cfg        = json_decode(json_encode(yaml_parse_file($_SERVER['HOME'] . '/config/yaml/config-repo.yaml')));
-$pdo        = new PDO($cfg->dbConn->guest);
+$cfg        = json_decode(json_encode(yaml_parse_file('/home/www-data/config/yaml/config-repo.yaml')));
+$pdo        = new PDO($cfg->dbConn->admin);
 $schemaDump = 'dump_schema.sql';
 $dataDump   = 'dump_data.sql';
 $imgsDump   = 'dump_titleimgages.tar';
 $usersDump  = 'dump_users.sql';
 $cfgDump    = 'dump_config.yaml';
 
+$dbConnParam = array_map(fn($x) => explode('=', $x), explode(' ', $cfg->dbConnStr->admin));
+$dbConnParam = array_combine(array_map(fn($x) => $x[0], $dbConnParam), array_map(fn($x) => $x[1] ?? '', $dbConnParam));
+$dbConn = '' .
+    (isset($dbConnParam['host']) ? " -h '" . $dbConnParam['host'] . "'" : '') .
+    (isset($dbConnParam['port']) ? " -p '" . $dbConnParam['port'] . "'" : '') .
+    (isset($dbConnParam['user']) ? " -U '" . $dbConnParam['user'] . "'" : '') .
+    (isset($dbConnParam['dbname']) ? " '" . $dbConnParam['dbname'] . "'" : '') ;
+
 if ($argv[1] === 'dump') {
     $basePath = $cfg->storage->dir;
     $levelMax = $cfg->storage->levels;
 
     function getStorageDir(int $id, string $path, int $level, int $levelMax): string {
-        if ($level < $levelMax) {
-            $path = sprintf('%s/%02d', $path, $id % 100);
-            $path = getStorageDir((int) $id / 100, $path, $level + 1, $levelMax);
+	$idTmp = $id;
+        while ($level < $levelMax) {
+            $path .= sprintf('/%02d', $idTmp % 100);
+            $idTmp = (int) $idTmp / 100;
+            $level++;
         }
-        return $path;
+        return $path . '/' . $id;
     }
 
     echo "Copying repository config\n";
-    copy($_SERVER['HOME'] . '/config/yaml/config-repo.yaml', "$basePath/$cfgDump");
+    copy('/home/www-data/config/yaml/config-repo.yaml', "$basePath/$cfgDump");
     echo "Dumping schema\n";
-    system('pg_dump -f ' . escapeshellarg("$basePath/$schemaDump") . ' -C -s');
+    #system('pg_dump -f ' . escapeshellarg("$basePath/$schemaDump") . ' -C -s ' . $dbConn);
     echo "Dumping metadata\n";
-    system('pg_dump -f ' . escapeshellarg("$basePath/$dataDump") . ' -a -T metadata_history -T users');
+    #system('pg_dump -f ' . escapeshellarg("$basePath/$dataDump") . ' -a -T metadata_history -T users ' . $dbConn);
 
     echo "Dumping title images\n";
-    $query = $pdo->prepare("SELECT id FROM relations WHERE property = ? ORDER BY 1");
-    $query->execute([$cfg->schema->titleImage]);
+    $query = $pdo->prepare("SELECT id FROM relations r JOIN metadata m USING (id) WHERE r.property = ? AND m.property = ? ORDER BY 1");
+    $query->execute([$cfg->schema->titleImage, $cfg->schema->hash]);
     $imgs = '';
     while($id = $query->fetchColumn()) {
-        $imgs .= " " . escapeshellarg(getStorageDir($id, '', 0, $levelMax));
+        $imgs .= " " . escapeshellarg(getStorageDir($id, '.', 0, $levelMax));
     }
     chdir($basePath);
-    escapeshellarg('tar -c -f ' . escapeshellarg("$basePath/$imgsDump") . $imgs);
+    system('tar -c -f ' . escapeshellarg("$basePath/$imgsDump") . $imgs);
     echo "\nDump completed - copy all the $basePath/dump_* files to the curation instance and run the script with the 'restore' parameter there.\n\n";
 } else {
     foreach ([$schemaDump, $dataDump, $imgsDump, $cfgDump] as $i) {
