33ccb7ad6c92fe26b8c675915c9e46de3de050d2
Mateusz Żółtak <zozlak@zozlak.org>
2025-06-06 20:55:54 +0000

    Tune the config to the non-cluster setup

--- backup.sh
+++ backup.sh
@@ -11,7 +11,7 @@ COMPRESSION="none"
 COMPRESSIONLEVEL=9
 CHUNKSIZEMB=204800 # 200GB
 
-. "$CDIR/cluster_init.sh"
+#. "$CDIR/cluster_init.sh"
 
 echo "### Initialization completed, starting a backup on `date +%Y-%m-%dT%H:%M:%S`"
 
