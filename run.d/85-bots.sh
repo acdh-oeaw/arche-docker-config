08bfcdaca0ded0ce2dd9bdc71684464d4d3883fd
Mateusz Żółtak <zozlak@zozlak.org>
2026-01-19 15:34:04 +0100

    run.d/85-bots.sh: fix the download URL

--- run.d/85-bots.sh
+++ run.d/85-bots.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 curl https://raw.githubusercontent.com/ai-robots-txt/ai.robots.txt/refs/heads/main/robots.txt > /home/www-data/docroot/robots.txt &&\
   cp /home/www-data/docroot/robots.txt /home/www-data/docroot/resolver/robots.txt
-curl https://github.com/ai-robots-txt/ai.robots.txt/blob/main/.htaccess > /etc/apache2/conf-available/bots.conf
+curl https://raw.githubusercontent.com/ai-robots-txt/ai.robots.txt/refs/heads/main/.htaccess > /etc/apache2/conf-available/bots.conf
