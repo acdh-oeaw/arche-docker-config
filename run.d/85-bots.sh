24014a3f4a91c7648092924ad22b95aa361c0c89
Mateusz Żółtak <zozlak@zozlak.org>
2026-05-04 11:34:59 +0200

    run.d/85-bots.sh: fix a typo

--- run.d/85-bots.sh
+++ run.d/85-bots.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 curl -f https://raw.githubusercontent.com/ai-robots-txt/ai.robots.txt/refs/heads/main/robots.txt > /tmp/robots.txt &&\
   cp /tmp/robots.txt /home/www-data/docroot/robots.txt &&\
-  cp /tmp/robots.txt /home/www-data/docroot/resolver/robots.txta &&\
+  cp /tmp/robots.txt /home/www-data/docroot/resolver/robots.txt &&\
   rm -f /tmp/robots.txt
 curl -f https://raw.githubusercontent.com/ai-robots-txt/ai.robots.txt/refs/heads/main/.htaccess > /tmp/htaccess &&\
   cp /tmp/htaccess /etc/apache2/conf-available/bots.conf &&\
