74f482d6a531f6cc5f81351092d7465bf05314ce
Mateusz Żółtak <zozlak@zozlak.org>
2026-05-04 16:05:58 +0200

    run.d/85-bots.sh: allow openaire despite matching openai :)

--- run.d/85-bots.sh
+++ run.d/85-bots.sh
@@ -4,5 +4,6 @@ curl -f https://raw.githubusercontent.com/ai-robots-txt/ai.robots.txt/refs/heads
   cp /tmp/robots.txt /home/www-data/docroot/resolver/robots.txt &&\
   rm -f /tmp/robots.txt
 curl -f https://raw.githubusercontent.com/ai-robots-txt/ai.robots.txt/refs/heads/main/.htaccess > /tmp/htaccess &&\
+  sed -i '/RewriteEngine On/a RewriteCond %{HTTP_USER_AGENT} !OpenAIRE [NC]' /tmp/htaccess &&\
   cp /tmp/htaccess /etc/apache2/conf-available/bots.conf &&\
   rm -f /tmp/htaccess
