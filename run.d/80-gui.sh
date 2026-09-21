c6ab4b48fdac811410b35bb38d9020eec60dd9c5
Norbert Czirjak <norbert.czirjak@oeaw.ac.at>
2026-09-21 16:16:36 +0200

    DB config added to the symfony env

--- run.d/80-gui.sh
+++ run.d/80-gui.sh
@@ -5,13 +5,20 @@ fi
 su -w http_proxy,https_proxy -l www-data -c 'cd /home/www-data/gui && APP_ENV=prod APP_DEBUG=0 composer install --no-dev --no-interaction --optimize-autoloader' || exit 1
 su -l www-data -c "ln -sfn /home/www-data/gui/public /home/www-data/docroot/guiApi"
 
+sed -i \
+  -e "s/^DB_USER=.*/DB_USER=$PG_USER/" \
+  -e "s/^DB_NAME=.*/DB_NAME=$PG_DBNAME/" \
+  -e "s/^DB_HOST=.*/DB_HOST=$PG_HOST/" \
+  -e "s/^DB_PORT=>*/DB_PORT=$PG_PORT/" \
+  -e "/^DB_PSWD=/d'
+  /var/www/hml/gui/.env
+
 CMD=/home/www-data/vendor/zozlak/yaml-merge/bin/yaml-edit.php
 CFGD=/home/www-data/config/yaml
 rm -f /home/www-data/gui/src/arche-config/config-gui.yaml $CFGD/config-gui.yaml
 su -l www-data -c "$CMD \\
     --src $CFGD/schema.yaml \\
     --src $CFGD/openaire.yaml \\
-    --src $CFGD/drupal.yaml \\
     --src $CFGD/smartSearch.yaml \\
     --src $CFGD/repo.yaml --srcPath '$.metadataManagment.nonRelationProperties' --targetPath '$.metadataManagment.nonRelationProperties' \\
     --src $CFGD/repo.yaml --srcPath '$.accessControl.publicRole' --targetPath '$.publicRole' \\
@@ -19,3 +26,4 @@ su -l www-data -c "$CMD \\
     --src $CFGD/local.yaml \\
     $CFGD/config-gui.yaml"
 su -l www-data -c "ln -s $CFGD/config-gui.yaml /home/www-data/gui/src/arche-config/config-gui.yaml"
+
